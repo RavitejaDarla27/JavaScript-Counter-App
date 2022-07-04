024729cfc052f742c8b5a68736ce00eff589dc85
RavitejaDarla27 <97012395+RavitejaDarla27@users.noreply.github.com>
2022-07-04 17:15:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const counterValue = document.querySelector('.counter-value')
+const btns = document.querySelectorAll('.btn')
+
+let count = 0;
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList
+        
+        if(styles.contains('increment')) {
+            count++;
+        } else if (styles.contains('decrement')) {
+            count--;
+        } else {
+            count = 0;
+        }
+        counterValue.textContent = count
+    });
+});
